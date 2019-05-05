941a4b0c04ba0d658c245f4563c710a782c4ca50
Oscar Guevara <guevara1697@gmail.com>
2019-05-05 18:23:02 -0500

    Login perros

--- models/users.js
+++ models/users.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -24,5 +27,47 @@ const userSchema = new mongoose.Schema({
 })
 
 const User = mongoose.model('User', userSchema)
+
+userSchema.statics.findByCredentials = function(email, password) {
+  return new Promise(function(resolve, reject) {
+    User.findOne({email}).then(function(user) {
+      if(!user) {
+        return reject('User does not exist.')
+      }
+      bcrypt.compare(password, user.password).then(function(match) {
+        return resolve(user)
+      }).catch(function(error) {
+        return reject('Wrong password.')
+      })
+    })
+  })
+}
+
+userSchema.methods.generateToken = function() {
+  const user = this
+  const token = jwt.sign({_id: user._id.toString()}, 'superSecret', {expiresIn: '7 days'})
+  user.tokens = user.tokens.concat({token})
+  return new Promise(function(resolve, reject) {
+    user.save().then(function(user) {
+      return resolve(token)
+    }).catch(function (error) {
+      return reject(error)
+    })
+  })
+}
+
+userSchema.pre('save', function(next) {
+  const user = this
+  if(user.isModified('password')) {
+    bcrypt.hash(user.password, 8).then(function(hash) {
+      user.password = hash
+      next()
+    }).catch(function(error) {
+      return next(error)
+    })
+  } else {
+    next()
+  }
+})
 
 module.exports = User
